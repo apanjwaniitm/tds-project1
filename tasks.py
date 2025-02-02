6f6f56039d78272399712c6f11158501e3e6b558
apanjwaniitm <24f1000189@ds.study.iitm.ac.in>
2025-02-02 17:42:42 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("AIRPROXY_TOKEN")
+openai.api_key = os.getenv("AIPROXY_TOKEN")
 
 # Task A2: Format Markdown files using Prettier
 import subprocess
